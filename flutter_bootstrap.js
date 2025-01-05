5f04d3e49755cc361a681ec53a4bffe6d253df25
Boumahdi Zeyd <zeyd.boumahdi@ecole.ensicaen.fr>
2025-01-05 12:39:47 +0100

    WESH

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3464148319"
+    serviceWorkerVersion: "3390306938"
   }
 });
