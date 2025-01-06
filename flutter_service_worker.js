83bb2d68531fae0c43c219e856dd7e0d649529e4
Boumahdi Zeyd <zeyd.boumahdi@ecole.ensicaen.fr>
2025-01-06 18:39:50 +0100

    HQJSHF

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "bda1f051026e359b1bb2ac8b317bbe59",
+const RESOURCES = {"flutter_bootstrap.js": "40e6279e51b500216958b55fa286d8e9",
 "version.json": "73caae75c5c3c73b4c946a26a0a3880c",
-"index.html": "b31a2e3579c139f8c46070b887c8ebff",
-"/": "b31a2e3579c139f8c46070b887c8ebff",
-"main.dart.js": "1baa62feba5427246d804e67af6bd75c",
+"index.html": "53f9e006cee883c92626f42ca197365a",
+"/": "53f9e006cee883c92626f42ca197365a",
+"main.dart.js": "b261a109f5a8223a0e0a5632159014fd",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
